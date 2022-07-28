68db4aa52fad1346b1db4147f77a9cd2b5855823
Kishore Kumar Barik <kishore@sails.solutions>
2022-07-28 12:38:41 +0530

    use login feature in app2

--- apps/app2/src/app/app.module.ts
+++ apps/app2/src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
+import { SharedFeatureLoginModule } from '@nx-angular-architecture/shared/feature/login';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent],
   imports: [
     BrowserModule,
+    SharedFeatureLoginModule,
     RouterModule.forRoot([], { initialNavigation: 'enabledBlocking' }),
   ],
   providers: [],
