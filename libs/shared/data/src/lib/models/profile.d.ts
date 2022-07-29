9dfef02846dfb2dbd65560c076f81ecd5e5bb2a0
Kishore Kumar Barik <kishore@sails.solutions>
2022-07-29 13:16:04 +0530

    added model

---
+++ libs/shared/data/src/lib/models/profile.d.ts
@@ -0,0 +1,10 @@
+export interface Profile {
+    username: string;
+    id: number;
+    firstname: string;
+    lastname: string;
+    email: string;
+    country: string;
+    token: string;
+    role: string;
+}
--- libs/shared/data/src/lib/shared-data.module.ts
+++ libs/shared/data/src/lib/shared-data.module.ts
@@ -1,5 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+export {ProfileService} from './services/profile.service';
+export {Profile} from './models/profile';
 
 @NgModule({
   imports: [CommonModule],
