81a13b2cbefc90c717b527e4f6e74683495ff198
Kishore Kumar Barik <kishore@sails.solutions>
2022-08-04 11:25:24 +0530

    shared header - dummy
    not customisable yet

--- libs/shared/ui/src/lib/shared-ui.module.ts
+++ libs/shared/ui/src/lib/shared-ui.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ButtonComponent } from './button/button.component';
 import { InputComponent } from './input/input.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   imports: [CommonModule],
-  declarations: [ButtonComponent, InputComponent],
-  exports: [ButtonComponent, InputComponent],
+  declarations: [ButtonComponent, InputComponent, HeaderComponent],
+  exports: [ButtonComponent, InputComponent, HeaderComponent],
 })
 export class SharedUiModule {}
