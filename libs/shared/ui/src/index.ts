3afcb70a057cffc4e2815bce78a61585719033cd
Kishore Kumar Barik <kishore@sails.solutions>
2022-08-04 11:25:54 +0530

    missed check in

--- libs/shared/ui/src/index.ts
+++ libs/shared/ui/src/index.ts
@@ -3,3 +3,5 @@ export * from './lib/shared-ui.module';
 export * from './lib/button/button.component';
 
 export * from './lib/input/input.component';
+
+export * from './lib/header/header.component';
