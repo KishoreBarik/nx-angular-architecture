1b8b7e7376728ee10607a7fecba79fec5517ca6e
Kishore Kumar Barik <kishore@sails.solutions>
2022-08-05 17:22:24 +0530

    refactors

--- apps/app1/src/app/app.module.ts
+++ apps/app1/src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { RouterModule } from '@angular/router';
-import { SharedFeatureLoginModule } from '@nx-angular-architecture/shared/feature/login';
+import { LoginComponent, SharedFeatureLoginModule } from '@nx-angular-architecture/shared/feature/login';
 import { App1DashboardModule, dashboardRoutes } from '@nx-angular-architecture/app1/dashboard';
 
 @NgModule({
@@ -12,7 +12,8 @@ import { App1DashboardModule, dashboardRoutes } from '@nx-angular-architecture/a
   imports: [
     BrowserModule,
     SharedFeatureLoginModule,
-    RouterModule.forRoot([{path: "dashboard", loadChildren: () => import('@nx-angular-architecture/app1/dashboard').then((module) => module.App1DashboardModule)}], { initialNavigation: "enabledNonBlocking" }),
+    RouterModule.forRoot([{path: "", pathMatch: "full", component: LoginComponent},
+    {path: "dashboard", loadChildren: () => import('@nx-angular-architecture/app1/dashboard').then((module) => module.App1DashboardModule)}], { initialNavigation: "enabledNonBlocking" }),
   ],
   providers: [],
   bootstrap: [AppComponent],
